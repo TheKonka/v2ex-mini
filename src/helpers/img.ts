c80b0d945b33f9a536492f0de7f87e80d3287291
konka <1094918@gmail.com>
2023-07-19 11:06:59 +0800

    优化图片显示

--- src/helpers/img.ts
+++ src/helpers/img.ts
@@ -4,14 +4,14 @@
  * @returns
  */
 export function getProxyImage(url: string) {
+	if (url.startsWith('https://i.328888.xyz/') || url.startsWith('https://i.niupic.com/')) {
+		return url;
+	}
 	if (url.startsWith('https://imgur.com/')) {
 		url = url.replace('https://imgur.com/', 'https://i.imgur.com/');
 	}
 	if (url.startsWith('https://i.imgur.com/')) {
 		return 'https://images.weserv.nl/?url=' + 'https://search.pstatic.net/common?src=' + url;
-	}
-	if (url.startsWith('https://i.328888.xyz/')) {
-		return url;
 	}
 
 	return 'https://images.weserv.nl/?url=' + url;
